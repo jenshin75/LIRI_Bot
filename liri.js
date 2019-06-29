2b77b9b7a186fb37c3b727cc1ae8659d957e72b7
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-06-29 13:27:17 -0500

    updated spotify 06/29/19 1:27pm

--- liri.js
+++ liri.js
@@ -3,78 +3,76 @@ var keys = require("./keys");
 
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
 var axios = require("axios");
-
-var spotify = require("node-spotify-api")
-var spotify = new Spotify(keys.spotify) 
-
-//======
-// function responseMusic() {
-//   var musicName = process.argv[3];
-// //   spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-// //     if (err) {
-// //       return console.log('Error occurred: ' + err);
-// //     }
-   
-// //   console.log(data); 
-// //   });
-// // }
-//=====
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
 // Grab the movieName which will always be the third node argument.
 var action = process.argv[2];
-var value = process.argv[3];
+//***var value = process.argv[3];
 
 switch (action) {
-case "movie-this":
-  responseMovie();
-break;
-
-case "concert-this":
-  responseConcert();
-break;
-
-case "spotify-this-song":
-  responseMusic();
-break;
+  case "movie-this":
+    responseMovie();
+    break;
+  case "concert-this":
+    responseConcert();
+    break;
+  case "spotify-this-song":
+    responseMusic();
+    break;
+  default: 
+    console.log("liri doesn't know...")
 }
 
 function responseMovie() {
-var movieName = process.argv[3];
-// Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  var movieName = process.argv[3];
+  // Then run a request with axios to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Movie Title: " + response.data.Title);
-    console.log("Release Year: " + response.data.Year);
-    console.log("IMDB Rating: " + response.data.imdbRating);
-    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-    console.log("Country Produced: " + response.data.Country);
-    console.log("Language: " + response.data.Language);
-    console.log("Plot: " + response.data.Plot);
-    console.log("Actors: " + response.data.Actors);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
+  axios.get(queryUrl).then(
+    function (response) {
+      console.log("Movie Title: " + response.data.Title);
+      console.log("Release Year: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+      console.log("Country Produced: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
+
+// console.log(keys.spotify);
+function responseMusic() {
+  var musicName = process.argv[3];
+
+  spotify.search({type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
     }
-    console.log(error.config);
-  });
+ console.log(data.tracks.items[0]); 
+ });
 }
